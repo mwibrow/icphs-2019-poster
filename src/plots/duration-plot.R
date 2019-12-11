851805b6ba0da51dac205f3082de7061696b1281
Mark Wibrow <m.wibrow@gmail.com>
2019-12-11 09:42:58 +0000

    Start tidying up plots

--- src/plots/duration-plot.R
+++ src/plots/duration-plot.R
@@ -1,36 +1,16 @@
 
 source(file.path("R", "settings.R"))
 
-
 dataDir <- "data"
 
-# vwl.df <- rbind(
-#     read.csv(file.path(dataDir, "pre-LV.csv")),
-#     read.csv(file.path(dataDir, "post-LV.csv")),
-#     read.csv(file.path(dataDir, "pre-HV.csv")),
-#     read.csv(file.path(dataDir, "post-HV.csv")))
-vwl.df = read.csv(file.path(dataDir, "duration-ssbe.csv"))
-vwl.df$Group = factor(vwl.df$Group, c('LV', 'HV', 'SSBE'))
-vwl.df$Test = factor(vwl.df$Test, c('pre', 'post', 'SSBE'))
+durDf = read.csv(file.path(dataDir, "duration-ssbe.csv"))
+durDf$Group = factor(durDf$Group, c("LV", "HV", "SSBE"))
+durDf$Test = factor(durDf$Test, c("pre", "post", "SSBE"))
 
 
-#pre.rows <- nrow(vwl.df)
-# vwl.df <- ddply(vwl.df, c("group", "test", "vowel"), function(d){
+colors$pre = "#999999"
+colors$ssbe = "#ffffff"
 
-#   duration.mad <- median(abs(d$duration - median(d$duration)))
-#   duration.dev <- abs(d$duration - median(d$duration)) / duration.mad
-#   cbind(d,
-#     data.frame(
-#       out=duration.dev> 3
-#     )
-#   )
-# })
-# vwl.df <- subset(vwl.df)#, !out)
-# post.rows <- nrow(vwl.df)
-# cat(sprintf("Removed %d outliers\n", pre.rows - post.rows))
-
-colors$pre = '#999999'
-colors$ssbe = '#bbbbbb'
 dpi <- 600
 width <- 3.333
 height <- 3.5
@@ -38,44 +18,56 @@ options(repr.plot.width=width, repr.plot.height=height)
 
 showtext_opts(dpi=dpi)
 fontSize <- 10
+fontFamily <- "DejaVuSans"
 
-p <- ggplot() + theme(
+theme_update(
   panel.background = element_blank(),
   panel.border = element_rect(colour = "#dddddd", fill=NA, size=1),
-        # panel.grid.major = element_blank(),
-        # panel.grid.minor = element_blank(),
-  #panel.background=element_rect(fill="transparent"),#colors$panel.background),
-  panel.grid.major=element_line(
-    color="#dddddd",#colors$panel.grid,
-    linetype="13",
-    lineend="round"),
-   text=element_text(family="DejaVuSans", size=fontSize),
-    axis.title.y = element_text(
-    margin=margin(t=0, r=1, b=0, l=0)),
-  axis.ticks=element_blank(),
-   axis.text.x = element_text(size=fontSize*0.75),
-   axis.title.x = element_text(hjust=0.333),
-    axis.text.y = element_text(size=fontSize*0.75),
-legend.position="bottom",
-legend.margin=margin(t=0,r=4,b=0,l=2),
+  panel.grid.major = element_line(
+    color = "#dddddd",
+    linetype = "13",
+    lineend = "round"
+  ),
+  text = element_text(
+    family = "DejaVuSans",
+    size = fontSize
+  ),
+  # Axis stuff
+  axis.title.y = element_text(
+    margin = margin(t=0, r=1, b=0, l=0)
+  ),
+  axis.ticks = element_blank(),
+  axis.text.x = element_text(size = fontSize*0.75),
+  axis.title.x = element_text(hjust = 0.333),
+  axis.text.y = element_text(size = fontSize*0.75),
+  # Legend stuff
+  legend.position = "bottom",
+  legend.margin = margin(t = 0, r = 4, b = 0, l = 2),
   legend.key=element_rect(
     fill="transparent",
-    colour="transparent"))
+    colour="transparent"
+  )
+)
+
+p <- ggplot()
 
 p <- p + geom_boxplot(
-  data=vwl.df, aes(x=Group, y=Duration, color=Test, fill=Test),
+  data=durDf, aes(x=Group, y=Duration, color=Test, fill=Test),
     varwidth = TRUE,
     outlier.shape=20)
+
+# This is a workaround to have the boxplot lines color and fill color
+# the same without generate extra legend entries.
 p <- p + scale_fill_manual(
     breaks=c("pre", "post"),
     labels=c("Pre", "Post"),
-    values=c(pre=colors$pre, post=colors$post, SSBE='#ffffff'),
+    values=c(pre=colors$pre, post=colors$post, colors=colors$ssbe),
     name="Test"
 )
 p <- p + scale_color_manual(
     breaks=c("pre", "post"),
     labels=c("Pre", "Post"),
-    values=c(pre=colors$pre, post=colors$post, SSBE=colors$post),
+    values=c(pre=colors$pre, post=colors$post, SSBE="#000000"),
     name="Test")
 
 p <- p + xlab("Group") +
@@ -88,16 +80,20 @@ p <- p + scale_y_continuous(
   labels=seq(0,600,150))
 
 # Add limits here to prevent filtering of data
-p <- p + coord_cartesian(ylim=c(0,600))
+p <- p + coord_cartesian(ylim=c(0, 600))
 
-dat <- ggplot_build(p)$data[[1]]
-dat$Test <- c("pre", "post", "pre", "post", "SSBE")
-dat$Group <- c("LV", "LV", "HV", "HV", "SSBE")
+# Medians are usually the same color as the lines of the boxplot
+# So to make them a different color have to do this...
+medDf <- ggplot_build(p)$data[[1]]
+medDf$Test <- c("pre", "post", "pre", "post", "SSBE")
+medDf$Group <- c("LV", "LV", "HV", "HV", "SSBE")
+medDf$Group <- factor(medDf$Group, levels=c("LV", "HV", "SSBE"))
 
-dat$Group <- factor(dat$Group, levels=c("LV", "HV", 'SSBE'))
-dat <- head(dat, 4)
+# Don't need SSBE as the median color is black for SSBE
+medDf <- subset(medDf, medDf$Group != "SSBE")
 p <- p + geom_segment(
-  data=dat,
+  data=medDf,
   aes(x=xmin, xend=xmax, y=middle, yend=middle), lineend="square", inherit.aes=FALSE, colour="white")
+
 
 ggsave(file.path(outDir, "Figure7.jpg"),device="jpeg",width=width, height=height, units="in", dpi=dpi)
